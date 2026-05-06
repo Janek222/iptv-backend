dbf471c477ee7bbf41310c8ef3c03d098d37b166
Janek222 <63614458+Janek222@users.noreply.github.com>
2026-05-06 22:42:47 +0200

    Update playlist.py

--- app/models/playlist.py
+++ app/models/playlist.py
@@ -1,10 +1,1 @@
-from sqlalchemy import Column, Integer, String
-# ✅ НЕ from app.models import Base, а:
-from app.core.database import Base
 
-class Playlist(Base):
-    __tablename__ = "playlists"
-
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String(255), nullable=False)
-    owner_email = Column(String(255), nullable=False, index=True)
